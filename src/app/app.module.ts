f5c4def961cc9988c259355614c082c7199754e8
vicenterg9 <vicenterg9@gmail.com>
2022-06-01 17:39:54 +0200

    SUBIDA1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraGiranteComponent } from './barra-girante/barra-girante.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraGiranteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    BarraGiranteComponent
+  ]
 })
 export class AppModule { }
